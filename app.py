be616dfb1ca1a7f2ced328bfbd94b7b8e703fd3b
102206502 <102206502@cc.ncu.edu.tw>
2018-05-07 22:34:34 +0800

    use template from course

--- app.py
+++ app.py
@@ -1,49 +1,35 @@
 from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi, WebhookHandler
+	LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+	InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+	MessageEvent, TextMessage, TextSendMessage,
 )
 
 app = Flask(__name__)
 
-# Channel Access Token
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-# Channel Secret
-handler = WebhookHandler('548f25858ec7905849359282d41a58ec')
+line_bot_api = LineBotApi('EgAE1+QO5FI8HJyWGLRF1YnvR8QwxWllSTotRSNv5oKUupyCRaXYw29/X1T05lOAQGLxtQxmVXsnHV0qjylujvDxhzX6X8as5XJRW/uiOJMHmsN00C6q21rfTkQN0U2zxvLqyaH2UVZ1BvRIeNDrzgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('bfe31fc18c99304ae7d16c6da16c99df')
 
-# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
+	signature = request.headers['X-Line-Signature']
+	body = request.get_data(as_text=True)
+	app.logger.info("Request body: " + body)
+	try:
+		handler.handle(body, signature)
+	except InvalidSignatureError:
+		abort(400)
+	return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        message)
-
-import os
+	line_bot_api.reply_message(
+		event.reply_token,
+		TextSendMessage(text=event.message.text))
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	app.run()
