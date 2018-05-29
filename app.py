31a5dbf81a80a8325f037fdc6ff455cb4fbb9b92
102206502 <102206502@cc.ncu.edu.tw>
2018-05-29 16:20:44 +0800

    test load csv(still echo)

--- app.py
+++ app.py
@@ -29,10 +29,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    discount_info_df = pd.read_csv('discount info.csv')
-    discount_info = discount_info_df['title', 'link']
+    discount_df = pd.read_csv('discount info.csv')
 	line_bot_api.reply_message(
 		event.reply_token,
-		TextSendMessage(text=str(discount_info)))
+		TextSendMessage(text=event.message.text))
 if __name__ == "__main__":
 	app.run()
