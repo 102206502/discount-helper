6d9cb8e5da6144962c103095f959f6f8f80ab8d4
102206502 <102206502@cc.ncu.edu.tw>
2018-05-29 16:24:48 +0800

    try fix indentation error

--- app.py
+++ app.py
@@ -30,8 +30,6 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     discount_df = pd.read_csv('discount info.csv')
-	line_bot_api.reply_message(
-		event.reply_token,
-		TextSendMessage(text=event.message.text))
+	line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 if __name__ == "__main__":
 	app.run()
