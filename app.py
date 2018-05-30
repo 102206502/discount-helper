7c2d593ebfd0562a135a2f17db405b2b8ea2efd2
102206502 <102206502@cc.ncu.edu.tw>
2018-05-30 17:23:36 +0800

    設貼文顯示數量限制

--- app.py
+++ app.py
@@ -76,10 +76,13 @@ def check_crawl():
 def get_discount_infoes(discount_df):
     discount_info = discount_df[['date','title','link']]
     re_msg = ''
+    max_lines = 25
     for i in range(len(discount_info)):
         info_meta = discount_info.iloc[i,:]
         link_str = urllib.parse.urljoin(crawler.domain, info_meta['link'])
         re_msg += info_meta['date']+'\n'+info_meta['title']+'\n'+link_str+'\n'
+        if i > max_lines:
+            break
     return re_msg
 
 if __name__ == "__main__":
